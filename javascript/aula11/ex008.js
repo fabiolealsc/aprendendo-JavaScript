f4d1e6474afc48b20c9f8ed9a984eb5c0fdc2d6d
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2021-08-18 15:50:28 -0300

    Add files via upload

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 30
+//Condição composta
+if (vel < 60){    
+    console.log(`A velocidade do seu carro é ${vel}`)
+}
+else {
+    console.log('Você passou a vel permitida, foi multado')
+}
+    console.log('Diriga sempre usando cinto de segurança!')
---
+++ javascript/aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 65
+console.log(`Você tem ${idade} anos`)
+if (idade < 16) {
+    console.log('Não Vota!')
+} else if (idade < 18 || idade > 60) {
+        console.log('Voto Opcional')
+} else{
+    console.log('Vota!')
+}
+    
+
